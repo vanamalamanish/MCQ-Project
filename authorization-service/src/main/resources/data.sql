812dd4b668ede3d4cbd580268fbab3feca78e86f
Vanamala Manish <vanamalamanish4@gmail.com>
2022-09-26 15:26:01 +0530

    initial commit

---
+++ authorization-service/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into user(id, name, password) values (1, 'admin1', 'abc1');
+insert into user(id, name, password) values (2, 'admin2', 'abc2');
+insert into user(id, name, password) values (3, 'admin3', 'abc3');
+
+
+
+insert into customer(id,name,password) values (100011,'manish','manish123');
+insert into customer(id,name,password) values (100012,'manikanta','manikanta123');
+insert into customer(id,name,password) values (100013,'soumya','soumya123');
+insert into customer(id,name,password) values (100014,'sinchana','sinchana123');
+insert into customer(id,name,password) values (100015,'akansha','akansha123');
+insert into customer(id,name,password) values (100016,'sarath','sarath');
+insert into customer(id,name,password) values (100017,'samvidha','samvidha');
---
+++ questions-service/src/test/java/com/cts/questions/QuestionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
