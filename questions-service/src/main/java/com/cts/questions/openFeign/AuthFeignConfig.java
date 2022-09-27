6e49003cc93b0d4898393b9e145785da59e1badc
Manikanta <50227418+Manikanta05@users.noreply.github.com>
2022-09-27 11:50:13 +0530

    updated

--- questions-service/src/main/java/com/cts/questions/openFeign/AuthFeignConfig.java
+++ questions-service/src/main/java/com/cts/questions/openFeign/AuthFeignConfig.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "http://localhost:8081/auth-service")
+@FeignClient(url = "http://localhost:8081/auth",name="auth-service")
 public interface AuthFeignConfig {
 
 	@GetMapping("/validate")
